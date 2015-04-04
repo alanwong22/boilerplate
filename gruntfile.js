8b7bb0468190400c068084a987140b317e002d70
Alan Wong <alanwong22@gmail.com>
2015-04-04 13:49:08 -0400

    boiler plate

---
+++ gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+//http-server /Users/Station22/Desktop/workspace/Angular-Sorting
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    bower: grunt.file.readJSON('./.bowerrc'),
+    concat: {
+      libs: {
+        src: [
+          '<%= bower.directory %>/angular/angular.min.js',
+          '<%= bower.directory %>/angular-route/angular-route.min.js',
+          '<%= bower.directory %>/jquery/dist/jquery.min.js'
+        ],
+        dest: 'app/js/lib.js'
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
+  grunt.registerTask('default', ['concat:libs']);
+
+};
